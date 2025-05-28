3bb72665690311f5f0629d5489527020cf6b1a2f
MohamedElsafty55 <moelsafty36@gmail.com>
2025-05-28 11:28:06 +0300

    first commit

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,57 @@
+import 'package:book_app/core/utils/assets.dart';
+import 'package:book_app/features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> sildingAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    super.initState();
+    sildingAnimation = Tween<Offset>(
+      end: Offset.zero,
+      begin: const Offset(0, 2),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            height: h * .16,
+            width: w * .8,
+            child: Image.asset(AssetsData.logo, fit: BoxFit.fill),
+          ),
+        ),
+        const SizedBox(height: 25),
+        SildingText(sildingAnimation: sildingAnimation),
+      ],
+    );
+  }
+}
